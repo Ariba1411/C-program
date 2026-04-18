4467d6cdaf84e513aa2d60f5fbfd503d151e9897
Ariba1411 <aribajan918@gmail.com>
2026-04-18 18:25:42 +0530

    Add files via upload

---
+++ printnumbers2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,i=1;
+    printf("Enter the number:");
+    scanf("%d",&a);
+    while(i<=a){
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
