9310de36d50e3e5e28bc2ca4b38ebecb3ee5bc9a
Ariba1411 <aribajan918@gmail.com>
2026-05-03 21:31:11 +0530

    Add files via upload

---
+++ pattern5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int a,i,j,k;
+    printf("Enter the number:");
+    scanf("%d",&a);
+    for(i=1;i<=a;i++){
+        for(k=1;k<=a-i;k++){
+            printf(" ");
+        }
+        for(j=1;j<= 2*i-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+        for(i=a-1;i>=1;i--){
+        for(k=1;k<=a-i;k++){
+            printf(" ");
+        }
+        for(j=1;j<=2*i-1;j++){
+            printf("*");
+        }
+        printf("\n");
+         }
+      return 0;
+        }
