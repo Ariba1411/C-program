e5fb0700ad1dac8c3f127a1e2c35faba4d7613e2
Ariba1411 <aribajan918@gmail.com>
2026-06-09 19:18:46 +0530

    Add files via upload

---
+++ primenumber.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+int a, i, count = 0;
+printf("Enter the number: ");
+scanf("%d", &a);
+for(i = 1; i <= a; i++) {
+if(a % i == 0) {
+count++;
+}
+}
+if(count == 2){
+printf("%d is a prime number.", a);
+}
+else{
+printf("%d is not a prime number.", a);
+}
+return 0;
+}
